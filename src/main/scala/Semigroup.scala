d4b1f344d0e75a1194241a14c340d767354c07ae
Jim Hunziker <jim.hunziker@konnect2.com>
2013-09-07 16:17:49 -0400

    semigroup demo

--- src/main/scala/Semigroup.scala
+++ src/main/scala/Semigroup.scala
@@ -1,6 +1,14 @@
 import scalaz._
 import Scalaz._
 
-object Semigroup {
-  def main(args: Array[String]) = println("hey")
+object AddMore extends Semigroup[Int] {
+  def append(a: Int, b: => Int): Int = a + b + 1
+}
+
+object SemigroupDemo {
+  def main(args: Array[String]) = {
+    println(AddMore.append(1, 2))
+    println(AddMore.apply.map(1)(x => 3))
+    println(AddMore.semigroupLaw.associative(1, 2, 3))
+  }
 }
